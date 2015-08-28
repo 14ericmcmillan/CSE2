41b9b2ff1de5eae6fdeb854bdb43d48a06174e49
eam218 <eam218@lehigh.edu>
2015-08-28 16:50:53 +0000

    updated Welcome Class

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,27 @@
+////////////////////////////////////////////////
+// Eric McMillan
+// hw01
+// Welcome Class Homework
+
+//  Define Class
+public class WelcomeClass {
+    
+//  Add Main Method
+    public static void main(String[] args) {
+
+//  Print out welcome message
+        System.out.println("    -----------");
+        System.out.println("    | WELCOME |");
+        System.out.println("    -----------");
+        
+// Print Network ID
+        System.out.println("  ^  ^  ^  ^  ^  ^");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+        System.out.println("<-E--A--M--2--1--8->");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+        System.out.println("  V  V  V  V  V  V");
+// Print biographical sentence
+        System.out.println("Last month I went on a trip to Yosemite National Park and climbed the Sentinel Dome.");
+        
+        }
+}
